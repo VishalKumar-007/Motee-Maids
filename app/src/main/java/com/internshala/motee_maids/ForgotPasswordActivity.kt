244e300f93ab919d48e81bb016d0572d3d007d44
Vishal <smartboyvishal30@gmail.com>
2022-03-30 15:01:35 +0530

    Added Forgot Password Screen

---
+++ app/src/main/java/com/internshala/motee_maids/ForgotPasswordActivity.kt
@@ -0,0 +1,14 @@
+package com.internshala.motee_maids
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class ForgotPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+
+    }
+}
--- app/src/main/java/com/internshala/motee_maids/LoginActivity.kt
+++ app/src/main/java/com/internshala/motee_maids/LoginActivity.kt
@@ -1,11 +1,23 @@
 package com.internshala.motee_maids
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var txtForgotPassword: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        txtForgotPassword = findViewById(R.id.txtForgotPassword)
+
+        txtForgotPassword.setOnClickListener {
+            val intent = Intent(this@LoginActivity, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

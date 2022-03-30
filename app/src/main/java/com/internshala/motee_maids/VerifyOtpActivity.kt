ca88c352a61c5e073b9d104d68ea7b2769b50812
Vishal <smartboyvishal30@gmail.com>
2022-03-31 00:35:59 +0530

    Completed Verify OTP Screen

--- app/src/main/java/com/internshala/motee_maids/VerifyOtpActivity.kt
+++ app/src/main/java/com/internshala/motee_maids/VerifyOtpActivity.kt
@@ -2,10 +2,20 @@ package com.internshala.motee_maids
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import com.chaos.view.PinView
 
 class VerifyOtpActivity : AppCompatActivity() {
+
+    lateinit var pinView: PinView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verify_otp)
+
+        pinView = findViewById(R.id.pinView)
+
+        pinView.requestFocus()
+
     }
 }

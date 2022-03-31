b599624b8d860613f63e398545df5d3f47707ede
MasoomMobiloitte <an-masoom@indicchain.com>
2022-03-31 13:48:22 +0530

    Added backButton functionality in every activity

--- app/src/main/java/com/internshala/motee_maids/ForgotPasswordActivity.kt
+++ app/src/main/java/com/internshala/motee_maids/ForgotPasswordActivity.kt
@@ -4,21 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.widget.Toolbar
 
 class ForgotPasswordActivity : AppCompatActivity() {
 
     lateinit var btnSubmit: Button
+    lateinit var backButtonForgot: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
 
         btnSubmit = findViewById(R.id.btnSubmit)
+        backButtonForgot = findViewById(R.id.backButtonForgot)
 
         btnSubmit.setOnClickListener {
             val intent = Intent(this@ForgotPasswordActivity, VerifyOtpActivity::class.java)
             startActivity(intent)
+        }
+
+        backButtonForgot.setOnClickListener {
+            onBackPressed()
         }
 
     }

f8a8fe10bef5d2534d905506a238b2b283d26f75
MasoomMobiloitte <an-masoom@indicchain.com>
2022-03-31 13:32:58 +0530

    Added SignUpActivity back button functionality

--- app/src/main/java/com/internshala/motee_maids/SignUpActivity.kt
+++ app/src/main/java/com/internshala/motee_maids/SignUpActivity.kt
@@ -1,11 +1,23 @@
 package com.internshala.motee_maids
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var backButtonSignUp: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        backButtonSignUp = findViewById(R.id.backButtonSignUp)
+
+        backButtonSignUp.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
